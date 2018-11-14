eecc1f9ba17004de76ee9f95214bd8f99a32d3d1
gwenaelle <gwenaelle@osau.re>
2018-11-14 18:41:35 +0000

    Added beginning of Request parsing and compare functions

--- src/reply.mli
+++ src/reply.mli
@@ -1,30 +1,3 @@
-module Request: sig
-  type standard_command = [
-    | `EHLO
-    | `HELO
-    | `MAIL
-    | `RCPT
-    | `DATA
-    | `RSET
-    | `NOOP
-    | `QUIT
-    | `VRFY
-  ]
-
-  type t = [
-    | standard_command
-    | `Other of string
-  ]
-
-  val pp: Format.formatter -> t -> unit
-
-  val compare: t -> t -> bool
-
-  val eval: string -> t
-
-  val command: t -> string
-end
-
 module Reply: sig
   (* Permanent positive response *)
   type pos_completion = [
