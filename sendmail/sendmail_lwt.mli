541b1c759255b90df074211472a97a61bc393816
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-07-28 21:37:56 +0200

    Update to X509.0.7.0

--- sendmail/sendmail_lwt.mli
+++ sendmail/sendmail_lwt.mli
@@ -7,7 +7,7 @@ val run :
   hostname:'a Domain_name.t ->
   ?port:int ->
   domain:Colombe.Domain.t ->
-  authenticator:X509.Authenticator.a ->
+  authenticator:X509.Authenticator.t ->
   from:Colombe.Reverse_path.t ->
   recipients:Colombe.Forward_path.t list ->
   Auth.authenticator option ->
