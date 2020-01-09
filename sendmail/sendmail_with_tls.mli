d96e69fb26e06be9cae189c2510c160dd948c941
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-01-09 13:52:21 +0100

    Functorize the way to add STARTTLS in-fly layer

--- sendmail/sendmail_with_tls.mli
+++ sendmail/sendmail_with_tls.mli
@@ -11,6 +11,42 @@ module Context_with_tls : sig
   val make : unit -> t
 end
 
+module type VALUE = sig
+  type 'x send
+  type 'x recv
+
+  type error = private [> Decoder.error | Encoder.error ]
+
+  val pp_error : error Fmt.t
+
+  val encode_without_tls : Encoder.encoder -> 'x send -> 'x -> (unit, error) State.t
+  val decode_without_tls : Decoder.decoder -> 'x recv -> ('x, error) State.t
+end
+
+module Make_with_tls (Value : VALUE) : sig
+  type 'x send = 'x Value.send
+  type 'x recv = 'x Value.recv
+
+  type error =
+    [ Encoder.error
+    | Decoder.error
+    | `Protocol of Value.error
+    | `Tls_alert of Tls.Packet.alert_type
+    | `Tls_failure of Tls.Engine.failure ]
+
+  type encoder = Context_with_tls.t
+  type decoder = Context_with_tls.t
+
+  val starttls_as_client : encoder -> Tls.Config.client -> (unit, error) State.t
+  val starttls_as_server : decoder -> Tls.Config.server -> (unit, error) State.t
+
+  val encode : encoder -> 'a send -> 'a -> (unit, error) State.t
+  val decode : decoder -> 'a recv -> ('a, error) State.t
+end
+
+(* {b Note.} Even if [Make_with_tls.encoder = Make_with_tls.decoder], idiomaticaly a server
+  will start to decoder and a client will start to encode. *)
+
 type domain = Domain.t
 type reverse_path = Reverse_path.t
 type forward_path = Forward_path.t
@@ -20,15 +56,9 @@ type authentication = Sendmail.authentication
 type ('a, 's) stream = ('a, 's) Sendmail.stream
 
 type error =
-  [ Request.Decoder.error
-  | Reply.Decoder.error
-  | `Unexpected_response of (int * string list)
-  | `Unsupported_mechanism
-  | `Encryption_required
-  | `Weak_mechanism
-  | `Authentication_rejected
-  | `Authentication_failed
-  | `Authentication_required
+  [ Decoder.error
+  | Decoder.error
+  | `Protocol of Sendmail.error
   | `Tls_alert of Tls.Packet.alert_type
   | `Tls_failure of Tls.Engine.failure ]
 
