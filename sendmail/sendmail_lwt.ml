21fd95f3110ee565c4d27d7aa5ed9a076d2bdaab
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-08-04 15:32:13 +0200

    Provide sendmail-with-starttls (instead of sendmail-with-tls)

--- sendmail/sendmail_lwt.ml
+++ sendmail/sendmail_lwt.ml
@@ -35,3 +35,27 @@ let sendmail ~hostname ?(port= 465) ~domain ~authenticator ?authentication sende
   Tls_lwt.connect authenticator (hostname, port) >>= fun (ic, oc) ->
   Sendmail.sendmail lwt rdwr { ic; oc; } ctx ~domain ?authentication sender recipients mail
   |> Lwt_scheduler.prj
+
+let failf fmt = Fmt.kstrf (fun err -> Lwt.fail (Failure err)) fmt
+
+let resolve host =
+  Lwt_unix.getprotobyname "tcp" >>= fun tcp ->
+  Lwt_unix.getaddrinfo host "submission" Unix.[ AI_PROTOCOL tcp.Unix.p_proto ] >>= function
+  | [] -> failf "Submission service is not recognized by your system"
+  | ai :: _ -> Lwt.return ai.ai_addr
+
+let sendmail_with_starttls ~hostname ?port ~domain ~authenticator ?authentication sender recipients mail =
+  resolve (Domain_name.to_string hostname) >>= fun addr ->
+  let addr = match addr, port with
+    | Unix.ADDR_INET (inet_addr, _), Some port -> 
+      Unix.ADDR_INET (inet_addr, port)
+    | _ -> addr in
+  let socket = Lwt_unix.socket (Unix.domain_of_sockaddr addr) Unix.SOCK_STREAM 0 in
+  Lwt_unix.connect socket addr >>= fun () ->
+  let ic = Lwt_io.of_fd ~mode:Lwt_io.Input socket in
+  let oc = Lwt_io.of_fd ~mode:Lwt_io.Output socket in
+  let tls = Tls.Config.client ~authenticator () in
+  let ctx = Sendmail_with_starttls.Context_with_tls.make () in
+  let mail () = Lwt_scheduler.inj (mail ()) in
+  Lwt_scheduler.prj (Sendmail_with_starttls.sendmail lwt rdwr { ic; oc; } ctx tls
+    ?authentication ~domain sender recipients mail)
