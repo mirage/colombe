d525211f5b7c630b8b955029e3e9e021952124cd
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-04-22 01:38:10 +0200

    Be compatible with tls.0.17.4

--- sendmail/tls_io.ml
+++ sendmail/tls_io.ml
@@ -41,60 +41,77 @@ module Make (Flow : FLOW) = struct
 
   type t = {
     socket : Flow.t;
-    mutable state : [ `Active of Tls.Engine.state | `Eof | `Error of error ];
+    mutable state : [ `Active of Tls.Engine.state
+                    | `Read_closed of Tls.Engine.state
+                    | `Write_closed of Tls.Engine.state
+                    | `Closed
+                    | `Error of error ];
     mutable linger : Cstruct.t list;
   }
 
   let fully_write socket ({ Cstruct.len; _ } as cs) =
     Flow.fully_write socket (Cstruct.to_string cs) 0 len
-    >>| R.reword_error (fun err -> Flow_error err)
 
   let read socket =
     let buf = Bytes.create 0x1000 in
-    Flow.read socket buf 0 (Bytes.length buf) >>= function
-    | Ok `End -> return `Eof
-    | Ok (`Len len) -> return (`Data (Cstruct.of_bytes ~off:0 ~len buf))
-    | Error err -> return (`Error (Flow_error err))
+    Flow.read socket buf 0 (Bytes.length buf) >>| function
+    | Ok `End -> Ok `Eof
+    | Ok (`Len len) -> Ok (`Data (Cstruct.of_bytes buf ~off:0 ~len))
+    | Error _ as err -> err
 
-  let check_write flow f_res =
-    ( match flow.state, f_res with
-      | `Active _, Error err ->
-          flow.state <- `Error err ; Flow.close flow.socket
-      | _ -> return () ) >>| fun () ->
-    match f_res with
-    | Ok ()   -> Ok ()
-    | Error e -> Error e
+  let half_close state mode = match state, mode with
+    | `Active tls, `read -> `Read_closed tls
+    | `Active tls, `write -> `Write_closed tls
+    | `Active _, `read_write -> `Closed
+    | `Read_closed tls, `read -> `Read_closed tls
+    | `Read_closed _, (`write | `read_write) -> `Closed
+    | `Write_closed tls, `write -> `Write_closed tls
+    | `Write_closed _, (`read | `read_write) -> `Closed
+    | (`Closed | `Error _) as e, (`read | `write | `read_write) -> e
+
+  let inject_state tls = function
+    | `Active _ -> `Active tls
+    | `Read_closed _ -> `Read_closed tls
+    | `Write_closed _ -> `Write_closed tls
+    | (`Closed | `Error _) as e -> e
+
+  let write_flow flow buf =
+    fully_write flow.socket buf >>= function
+    | Ok _ as o -> return o
+    | Error e ->
+      flow.state <- `Error (Flow_error e);
+      return (Error (Flow_error e))
 
   let read_react flow =
 
     let handle tls buf =
       match Tls.Engine.handle_tls tls buf with
-      | Ok (res, `Response resp, `Data data) ->
-          flow.state <- ( match res with
-            | `Ok tls      -> `Active tls
-            | `Eof         -> `Eof
-            | `Alert alert -> `Error (Alert alert)) ;
+      | Ok (state, eof, `Response resp, `Data data) ->
+          let state = inject_state state flow.state in
+          let state = Option.(value ~default:state (map (fun `Eof -> half_close state `read) eof)) in
+          flow.state <- state;
           ( match resp with
             | None -> return (Ok ())
-            | Some buf -> fully_write flow.socket buf >>= check_write flow) >>= fun _ ->
-          ( match res with
-            | `Ok _ -> return ()
-            | _ -> Flow.close flow.socket) >>= fun () ->
+            | Some buf -> write_flow flow buf) >>= fun _ ->
           return @@ `Ok data
       | Error (fail, `Response resp) ->
           let r = `Error (Failure fail) in
           flow.state <- r ;
-          fully_write flow.socket resp |> fun _ -> Flow.close flow.socket >>= fun () -> return r
+          fully_write flow.socket resp >>= fun _ -> return r
     in
     match flow.state with
-    | `Eof | `Error _ as e -> return e
-    | `Active _            ->
-      read flow.socket >>=
-      function
-      | `Eof | `Error _ as e -> flow.state <- e ; return e
-      | `Data buf            -> match flow.state with
-        | `Active tls          -> handle tls buf
-        | `Eof | `Error _ as e -> return e
+    | `Error _ as e -> return e
+    | `Read_closed _ | `Closed -> return `Eof
+    | `Active _ | `Write_closed _ ->
+      read flow.socket >>= function
+      | Error e -> flow.state <- `Error (Flow_error e); return (`Error (Flow_error e))
+      | Ok `Eof ->
+        flow.state <- half_close flow.state `read;
+        return `Eof
+      | Ok `Data buf -> match flow.state with
+        | `Active tls | `Write_closed tls -> handle tls buf
+        | `Read_closed _ | `Closed -> return `Eof
+        | `Error _ as e -> return e
 
   let rec read flow =
     match flow.linger with
@@ -110,13 +127,13 @@ module Make (Flow : FLOW) = struct
 
   let writev flow bufs =
     match flow.state with
-    | `Eof -> return (Result.Error Closed)
+    | `Closed | `Write_closed _ -> return (Result.Error Closed)
     | `Error err -> return (Result.Error err)
-    | `Active tls ->
+    | `Active tls | `Read_closed tls ->
         match Tls.Engine.send_application_data tls bufs with
         | Some (tls, answer) ->
             flow.state <- `Active tls ;
-            fully_write flow.socket answer >>= check_write flow
+            write_flow flow answer
         | None ->
             (* "Impossible" due to handshake draining. *)
             assert false
@@ -124,12 +141,17 @@ module Make (Flow : FLOW) = struct
   let write flow cs = writev flow [ cs ]
 
   let close flow =
-    match flow.state with
-    | `Active tls ->
-      flow.state <- `Eof ;
-      let _, buf = Tls.Engine.send_close_notify tls in
+    ( match flow.state with
+    | `Active tls | `Read_closed tls ->
+      let tls, buf = Tls.Engine.send_close_notify tls in
+      flow.state <- inject_state tls flow.state;
+      flow.state <- `Closed;
       fully_write flow.socket buf >>= fun _ -> return ()
-    | _ -> return ()
+    | `Write_closed _ ->
+      flow.state <- `Closed;
+      return ()
+    | _ -> return () ) >>= fun () ->
+    Flow.close flow.socket
 
   let rec drain_handshake flow =
     match flow.state with
